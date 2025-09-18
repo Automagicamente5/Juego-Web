6476a0aca407fe281d461e6b52ce986c9318b33f
Erick <erick.nahuelpaiva.001@gmail.com>
2025-09-18 19:05:07 -0300

    modularizado

--- app.js
+++ app.js
@@ -1,57 +1,103 @@
+const ID_TABLERO_HTML = "#tablero";
+const MAX_FILAS = 10;
+const MAX_COL = 10;
+const SIMB_CASILLA_DEF = "";
+
+const MOVIMIENTO_ARRIBA = "w";
+const MOVIMIENTO_IZQUIERDA = "a";
+const MOVIMIENTO_ABAJO = "s";
+const MOVIMIENTO_DERECHA = "d";
+
 const jugador = {
     posX: 0,
     posY: 0,
-    simbolo: "J"
+    simbolo: "🤖"
 }
 
-function crearTablero() {
-    let divTablero = document.querySelector("#tablero");
+/**
+ * Inicia el programa de la app web
+ */
+function main() {
+    crearTablero();
+    document.addEventListener('keydown', manejarEventoTeclado);
+}
 
-    for (let fila = 0; fila < 10; fila++) {
-        divTablero.innerHTML += `<div id="fila-${fila}" class="fila"></div>`
-        for (let col = 0; col < 10; col++) {
-            document.querySelector(`#fila-${fila}`).innerHTML += `
-                <div id="pos-${fila}-${col}" class="casilla">
-                    <p></p>
-                </div>
-            `;
+main();
+
+/**
+ * Crea el tablero en la app web
+ */
+function crearTablero() {
+    let divTablero = document.querySelector(ID_TABLERO_HTML);
+    for (let fila = 0; fila < MAX_FILAS; fila++) {
+        divTablero.innerHTML += generarFilaHtml(fila);
+        for (let col = 0; col < MAX_COL; col++) {
+            agregarCasilla(fila, col);
         }
     }
-
-    document.querySelector(`#pos-${jugador.posY}-${jugador.posX}>p`).innerHTML = jugador.simbolo;
+    actualizarCasillaJugadorHTML(jugador.simbolo);
 }
-
-crearTablero()
-
-
 
 
 /**
+ * Agrega una casilla en la fila y columna correspondiente
+ * @param {Number} numFila a agregar casilla
+ * @param {Number} numCol a agregar casilla
+ */
+function agregarCasilla(numFila, numCol) {
+    document.querySelector(`#fila-${numFila}`).innerHTML += `
+                <div id="pos-${numFila}-${numCol}" class="casilla">
+                    <p></p>
+                </div>
+            `;
+}
+
+/**
+ * Genera el html para una fila
+ * @param {Number} numFila actual a generar
+ * @returns html necesario para generar una fila
+ */
+function generarFilaHtml(numFila) {
+    return `<div id="fila-${numFila}" class="fila"></div>`;
+}
+
+/**
  * Detecta la presion de alguna tecla
- * @param {Event} evento de presionar alguna tecla
+ * @param {KeyboardEvent} evento de presionar alguna tecla
  */
 function manejarEventoTeclado(evento) {
     const caracterPresionado = evento.key;
-    console.log(`Se ha presionado la tecla: ${caracterPresionado}`);
+    actualizarCasillaJugadorHTML(SIMB_CASILLA_DEF);
+    actualizarPosJugador(caracterPresionado);
+    actualizarCasillaJugadorHTML(jugador.simbolo);
+}
 
-    document.querySelector(`#pos-${jugador.posY}-${jugador.posX}>p`).innerHTML = "";
-    switch (caracterPresionado) {
-        case "w":
+
+/**
+ * Actualiza las coordenadas del jugador
+ * @param {String} caracterPresionado por el usuario
+ */
+function actualizarPosJugador(caracterPresionado) {
+    switch (caracterPresionado.toLowerCase()) {
+        case MOVIMIENTO_ARRIBA:
             jugador.posY--;
             break;
-        case "a":
+        case MOVIMIENTO_IZQUIERDA:
             jugador.posX--;
             break;
-        case "s":
+        case MOVIMIENTO_ABAJO:
             jugador.posY++;
             break;
-        case "d":
+        case MOVIMIENTO_DERECHA:
             jugador.posX++;
             break;
     }
-    document.querySelector(`#pos-${jugador.posY}-${jugador.posX}>p`).innerHTML = jugador.simbolo;
-
 }
 
-// Agrega el listener al input
-document.addEventListener('keydown', manejarEventoTeclado);
+/**
+ * Actualiza la casilla del jugador con el nuevoSimbolo
+ * @param {String} nuevoSimbolo a usar para actualizar la casilla del jugador
+ */
+function actualizarCasillaJugadorHTML(nuevoSimbolo = "def") {
+    document.querySelector(`#pos-${jugador.posY}-${jugador.posX}>p`).innerHTML = nuevoSimbolo;
+}
